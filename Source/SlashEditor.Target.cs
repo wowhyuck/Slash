42e1496ec0e18ad26d18adb2f699b6c09eb11619
wowhyuck <joohyuck.seo@gmail.com>
2024-01-01 22:21:05 +0900

    Initial commit

---
+++ Source/SlashEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class SlashEditorTarget : TargetRules
+{
+	public SlashEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "Slash" } );
+	}
+}
