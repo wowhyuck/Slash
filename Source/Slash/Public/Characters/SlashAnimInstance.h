7ec91ea7834d670ea6be27cc6af91409dd8d6614
joohyuck <joohyuck.seo@gmail.com>
2024-11-18 22:08:19 +0900

    Switching Animation Poses

--- Source/Slash/Public/Characters/SlashAnimInstance.h
+++ Source/Slash/Public/Characters/SlashAnimInstance.h
@@ -31,5 +31,6 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category = Movement)
 	bool IsFalling;
 
+	UPROPERTY(BlueprintReadOnly, Category = "Movement | Character State")
 	ECharacterState CharacterState;
 };
