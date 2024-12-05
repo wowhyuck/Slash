ed758ee603937e9ceb5c17982bf9bc387c465cd1
joohyuck <joohyuck.seo@gmail.com>
2024-12-06 03:18:03 +0900

    Creating Fields with Weapons

--- Source/Slash/Private/Items/Weapons/Weapon.cpp
+++ Source/Slash/Private/Items/Weapons/Weapon.cpp
@@ -107,5 +107,7 @@ void AWeapon::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* Oth
 			HitInterface->GetHit(BoxHit.ImpactPoint);
 		}
 		IgnoreActors.AddUnique(BoxHit.GetActor());
+
+		CreateFields(BoxHit.ImpactPoint);
 	}
 }
