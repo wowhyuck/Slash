aa1811fea0a6c40a5d57aed523458c5ded016526
joohyuck <joohyuck.seo@gmail.com>
2024-12-12 11:14:23 +0900

    Death Poses

--- Source/Slash/Public/Enemy/Enemy.h
+++ Source/Slash/Public/Enemy/Enemy.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Interfaces/HitInterface.h"
+#include "Characters/CharacterTypes.h"
 #include "Enemy.generated.h"
 
 class UAnimMontage;
@@ -34,6 +35,9 @@ protected:
 	* Play montage functions
 	*/
 	void PlayHitReactMontage(const FName& SectionName);
+
+	UPROPERTY(BlueprintReadOnly)
+	EDeathPose DeathPose = EDeathPose::EDP_Alive;
 
 private:
 	UPROPERTY(VisibleAnywhere)
