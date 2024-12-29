aa914e39c0a5fc1b826cb9d7635b53ce6e609819
joohyuck <joohyuck.seo@gmail.com>
2024-12-30 03:40:05 +0900

    Stop Enemy Attacks

--- Source/Slash/Private/Enemy/Enemy.cpp
+++ Source/Slash/Private/Enemy/Enemy.cpp
@@ -110,8 +110,10 @@ void AEnemy::Die()
 
 void AEnemy::Attack()
 {
-	EnemyState = EEnemyState::EES_Engaged;
 	Super::Attack();
+	if (CombatTarget == nullptr) return;
+
+	EnemyState = EEnemyState::EES_Engaged;
 	PlayAttackMontage();
 }
 
