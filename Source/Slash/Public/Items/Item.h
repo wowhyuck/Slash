973502f7f0582f83b8f67cb0b5a30606b35506d2
joohyuck <joohyuck.seo@gmail.com>
2025-03-14 23:25:33 +0900

    Task Progress

--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -35,6 +35,9 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
 	float TimeConstant = 5.f;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+	FName QuestID;
+
 	UFUNCTION(BlueprintCallable)
 	float TransformedSin();
 
@@ -44,7 +47,7 @@ protected:
 	template<typename T>
 	T Avg(T First, T Second);
 
-	UFUNCTION()
+	UFUNCTION(BlueprintCallable)
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	UFUNCTION()
